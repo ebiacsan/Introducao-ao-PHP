db6377cc48d640f83923b11cedf706936ddca7b8
Bianca Csan <ebcsan42@hotmail.com>
2020-04-13 05:33:32 +0000

    Create: Tipos de dados

---
+++ Curso-basico-1/TiposDeDados.php
@@ -0,0 +1,10 @@
+<?php
+
+$idade = 50;        //integer
+$salario = 1000.30; //double
+$divisao = 100/3;   //double
+$texto = "Hello";   //string
+$verdadeiro = true; //boolean
+$falso = false;     //boolean
+
+echo gettype($verdadeiro), "\n";
