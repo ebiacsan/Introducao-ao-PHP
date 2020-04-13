75d3e9be7e1db0c21daf25e278334a323ec40458
Bianca Csan <ebcsan42@hotmail.com>
2020-04-13 05:35:24 +0000

    File: String.php

---
+++ Curso-basico-1/Strings.php
@@ -0,0 +1,10 @@
+<?php
+
+$idade = 50;
+
+echo "Hello World!" . PHP_EOL . "Minha idade é: $idade \n";
+/*
+ * --> O .PHP_EOL . é o equivalente ao \n = faz quebra de linha e o EOL pode ser
+ *     melhor, pois ele leva em consideração o caracter de quebra de linha em
+ *     diferentes S.O's
+*/
